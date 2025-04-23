677daf1d92017dfb5a32f3f774fd7c90e81c1acc
Székely Máté <szekelymate60@gmail.com>
2025-04-23 15:36:41 +0200

    implementing tectons

---
+++ Prototype/src/MyceliumSustainerTecton.java
@@ -0,0 +1,41 @@
+package fungorium;
+
+public class MyceliumSustainerTecton extends Tecton{
+
+	private static final String type = "mycelium sustainer tecton";
+
+    public MyceliumSustainerTecton(int precentToBreak, String tectonName, TectonView view) { super(precentToBreak, tectonName, type, view); }
+
+    @Override
+    public void addMycelium(Mycelium m) throws Exception { 
+        myceliumList.add(m); 
+
+        view.myceliumAdded(this, m);
+    }
+
+    @Override
+    public void roundPassed() {
+        boolean tectonBreaks = generatedNumWithinBound(breakPrecent);
+
+        if (tectonBreaks) { breakTecton(); }
+        for (Mycelium m : myceliumList){
+            m.increaseRoundsToLive();
+        }
+    }
+
+    @Override
+    public boolean canPlaceBody() throws Exception{
+        if (!canPlaceBodyHelper()) throw new Exception(view.alreadyHasFungusbody(this));
+        return true;
+    }
+
+    @Override
+    public void breakTecton() {
+        Tecton newTecton = new OrdinaryTecton(breakPrecent, name + "-2", view);
+        removeConnectionAtBreak();
+        manageNeighboursAtBreak(newTecton);
+
+        view.tectonBreaks(this, newTecton);
+    }
+    
+}
---
+++ Prototype/src/SingleMyceliumTecton.java
@@ -0,0 +1,38 @@
+package fungorium;
+
+public class SingleMyceliumTecton extends Tecton {
+
+    private static final String type = "single mycelium tecton";
+
+    public SingleMyceliumTecton(int precentToBreak, String tectonName, TectonView view) { super(precentToBreak, tectonName, type, view); }
+
+    @Override
+    public void addMycelium(Mycelium m) throws Exception{ 
+
+        if (myceliumList.isEmpty()) { myceliumList.add(m); }
+        else { throw new Exception(view.singleMyceliumTectonAlreadyHasMycelium(this)); }
+    }
+
+    @Override
+    public void roundPassed() {
+        boolean tectonBreaks = generatedNumWithinBound(breakPrecent);
+
+        if (tectonBreaks) { breakTecton(); }
+    }
+
+    @Override
+    public boolean canPlaceBody() throws Exception {
+        if (!canPlaceBodyHelper()) throw new Exception(view.alreadyHasFungusbody(this));
+        return true;
+    }
+
+    @Override
+    public void breakTecton() {
+        Tecton newTecton = new SingleMyceliumTecton(breakPrecent, name + "-2", view);
+        removeConnectionAtBreak();
+        manageNeighboursAtBreak(newTecton);
+
+        view.tectonBreaks(this, newTecton);
+
+    }
+}
