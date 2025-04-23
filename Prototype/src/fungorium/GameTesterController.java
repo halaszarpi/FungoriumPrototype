cddbaaaa26d9f3a10f87e627899fb7e0eb396d81
Halász Árpád Armand <halaszarpi0507@gmail.com>
2025-04-23 17:19:58 +0200

    tester branch

--- Prototype/src/fungorium/GameTesterController.java
+++ Prototype/src/fungorium/GameTesterController.java
@@ -1,7 +1,35 @@
 package fungorium;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class GameTesterController {
 
-    // TODO
+    private Scanner scanner;
+    private GameTesterView view;
 
+    public GameTesterController(Scanner scanner) {
+        this.scanner = scanner;
+        view = new GameTesterView();
+    }
+
+    public void printMenuAndWaitForOption() {
+        view.listGameTesterOptions();
+        int option = scanner.nextInt();
+        switch(option) {
+            case 1 -> createTest();
+            case 2 -> runTest();
+            case 3 -> {return;}
+            default -> System.out.println("Invalid option");
+        }
+    }
+
+    public void createTest() {
+        view.enterTestName();
+        String testName = scanner.nextLine();
+        File filePath = new File(System.getProperty("user.dir") + "\\Prototype\\src\\tests");
+        File testFile = new File(filePath, testName + ".txt");
+    }
+
+    public void runTest() {}
 }
--- Prototype/src/fungorium/Main.java
+++ Prototype/src/fungorium/Main.java
@@ -8,55 +8,11 @@ public class Main {
     public static void main(String[] args) {
 
         scanner = new Scanner(System.in);
-        boolean exit = false;
-        String input;
 
-        while(!exit){
-            print_mainMenu();
+        GameTesterController gtc = new GameTesterController(scanner);
+        gtc.printMenuAndWaitForOption();
 
-            input = scanner.nextLine().toUpperCase();
-
-            switch (input) {
-                case "1" -> newGame(scanner);
-                case "2" -> gameTester(scanner);
-                case "E" -> {System.out.println("Goodbye!"); exit = true;}
-                default -> System.out.println("Invalid input!");
-            }
-        }
         scanner.close();
     }
 
-    private static void newGame(Scanner scanner) {
-        GameController gamecontroller = new GameController(scanner);
-    }
-
-    /// TODO
-    private static void gameTester(Scanner scanner) {
-       TesterController testercontroller = new TesterController(scanner);
-    }
-
-    private static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
-    private static void print_Logo(){
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("|  #####  #   #  #   #  #####  #####  #####  #  #   #  #   #  |");
-        System.out.println("|  #      #   #  ##  #  #      #   #  #   #  #  #   #  ## ##  |");
-        System.out.println("|  ####   #   #  # # #  #  ##  #   #  ###    #  #   #  # # #  |");
-        System.out.println("|  #      #   #  #  ##  #   #  #   #  #  ##  #  #   #  #   #  |");
-        System.out.println("|  #      #####  #   #  #####  #####  #   #  #  #####  #   #  |");
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("\n");
-    }
-
-    private static void print_mainMenu() {
-        clearScreen();
-        print_Logo();
-        System.out.println("Main Menu:");
-        System.out.println("1 - New Game");
-        System.out.println("2 - Game Tester");
-        System.out.println("E - Exit");
-    }
 }
