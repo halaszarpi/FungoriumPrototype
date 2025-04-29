07706132b75e30b3bb37c0896f996bddd92c8799
Nemabel <144894176+Nemabel@users.noreply.github.com>
2025-04-29 19:53:19 +0200

    insectkeeper info updated

--- Prototype/src/fungorium/InsectKeeperView.java
+++ Prototype/src/fungorium/InsectKeeperView.java
@@ -24,7 +24,20 @@ public class InsectKeeperView {
         System.out.println("\n\tSCORE: "+insectKeeper.getScore());
         System.out.println("\nINSECTS ");
         for(Insect insect:insectKeeper.getInsects()){
-            System.out.println("\t"+insect.getName() + " on tecton (" + insect.getTecton().getName() + ")");                                                                                                                                                                                                                                                        }
+            System.out.println("\t"+insect.getName() + " on tecton (" + insect.getTecton().getName() + ")");
+            if(insect.getBoostedForRounds()!=0){
+                System.out.println("\t\tBoosted for "+insect.getBoostedForRounds()+" rounds");
+            }
+            if(insect.getSlowedForRounds()!=0){
+                System.out.println("\t\tSlowed for "+insect.getSlowedForRounds()+" rounds");
+            }
+            if(insect.getStunnedForRounds()!=0){
+                System.out.println("\t\tStunned for "+insect.getStunnedForRounds()+" rounds");
+            }
+            if(insect.getAntiSeveredForRounds()!=0){
+                System.out.println("\t\tAnti severed for "+insect.getAntiSeveredForRounds()+" rounds");
+            }
+        }
         System.out.println("-------------------");
 
     }
