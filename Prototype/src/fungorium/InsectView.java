cecf32178dbe6f0193f1bcd69ec8fabff25e98fc
Nemabel <144894176+Nemabel@users.noreply.github.com>
2025-05-18 22:01:07 +0200

    fixed mssing line

--- Prototype/src/fungorium/InsectView.java
+++ Prototype/src/fungorium/InsectView.java
@@ -105,4 +105,8 @@ public class InsectView implements IObserver {
         g.drawPolygon(p);
     }
 
+    public String cannotCutMyceliumMessage() {
+        String message = "Insect (" + insect.getName() + ") cannot cut mycelium because it is stunned!";
+        return message;
+    }
 }
