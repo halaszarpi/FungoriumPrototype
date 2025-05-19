7fe58ee4debc8151c69128af8debf2082c0589e2
Székely Máté <szekelymate60@gmail.com>
2025-05-19 16:33:25 +0200

    insectview-ba visszaraktam az insectDuplicated()-et

--- Prototype/src/fungorium/Insect.java
+++ Prototype/src/fungorium/Insect.java
@@ -144,6 +144,7 @@ public class Insect implements IRoundFollower {
      */
     public void duplicate() {
         Insect duplicatedInsect = owner.duplicateInsect(this);
+        view.insectDuplicated(duplicatedInsect);
     }
 
     /**
