d40724d00759072f9e92c15edb62956afdb7825c
Székely Máté <szekelymate60@gmail.com>
2025-05-20 19:13:04 +0200

    radius adjust

--- Prototype/src/fungorium/GMap.java
+++ Prototype/src/fungorium/GMap.java
@@ -21,7 +21,7 @@ public class GMap extends JPanel {
     private ArrayList<GTecton> gTectons;
     private GTecton chosenTecton;
     private List<GTecton> chosenTectonNeighbours;
-    private final int radius = 275;
+    private final int radius = 250;
 
     public GMap(TectonMap map) {
 
